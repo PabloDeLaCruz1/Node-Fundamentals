16585e8b5ed8c9610ec39f60f5d03c613a3e8467
Pablo De La Cruz <pdelac@ramapo.edu>
2020-11-29 14:30:43 -0500

    basic curl

--- commands.js
+++ commands.js
@@ -22,8 +22,12 @@ let commands = {
     echo: (data) => {
         process.stdout.write(data.slice(1).join(" ").toString());
     },
-    curl: () => {
-
+    curl: (link) => {
+        request(link[1], function (error, response, body) {
+            console.error('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the page.
+        });
     }
 }
 
